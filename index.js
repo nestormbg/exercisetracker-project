0deb013381d2a2aea05bcd871e2dad42d48a606c
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-24 17:40:20 -0300

    node version updated

--- index.js
+++ index.js
@@ -2,14 +2,47 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI);
+ 
+const UserSchema = new Schema({
+  username: String
+});
+
+const User = mongoose.model('User', UserSchema);
+
+const ExerciseSchema = new Schema({
+  user_id: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res) => {
+  const userObj = new User({
+    username: req.body.username
+  });
 
+  try {
+    const user = await userObj.save();
+    res.json(user);
+  } catch (error) {
+    res.json({ error });
+  }
+
+
+});
 
 
 
