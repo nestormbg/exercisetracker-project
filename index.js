a52c5c5f30adf8fb16533518e5429a6e4513db16
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-24 18:16:48 -0300

    exercise endpoint created

--- index.js
+++ index.js
@@ -40,6 +40,40 @@ app.post('/api/users', async (req, res) => {
   } catch (error) {
     res.json({ error });
   }
+});
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+
+  try {
+    const user = await User.findById(id);
+    
+    if (!user) {
+      res.send('Could not find user');
+    } else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration, 
+        date: date ? new Date(date) : new Date()
+      });
+
+      const exercise = await exerciseObj.save();
+
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString()
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.send('Error saving the exercise');
+  }
 
 
 });
