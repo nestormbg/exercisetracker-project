60046ac80cfcc2e8ea8e7ac4767021dbe503d50b
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-25 08:08:14 -0300

    logs implemented

--- index.js
+++ index.js
@@ -39,6 +39,51 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const { from, to, limit } = req.query;
+  const id = req.params._id;
+  const user = await User.findById(id);
+
+  if (!user) {
+    res.send('Could not find user');
+    return;
+  } 
+
+  let dateObj = {};
+
+  if (from) {
+    dateObj['$gte'] = new Date(from);
+  }
+
+  if (to) {
+    dateObj['$lte'] = new Date(to); 
+  }
+
+  let filter = {
+    user_id: id,
+  };
+
+  if (from || to) {
+    filter.date = dateObj;
+  }
+
+  const exercises = await Exercise.find(filter).limit(+limit ?? 500);
+
+  const log = exercises.map(e => ({
+    description: e.description,
+    duration: e.duration,
+    date: e.date.toDateString()
+  }));
+
+  res.json({
+    username: user.username,
+    count: exercises.length,
+    _id: user._id,
+    log 
+  })
+
+});
+
 app.post('/api/users', async (req, res) => {
   const userObj = new User({
     username: req.body.username
